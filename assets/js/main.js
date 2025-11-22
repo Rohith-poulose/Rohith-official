caedab4b705a341fd512eb8b928c80e97fff75f6
ROHITH_P <rohithinteractive14@gmail.com>
2025-11-22 17:45:30 +0530

    fix: insta username

--- assets/js/main.js
+++ assets/js/main.js
@@ -105,13 +105,6 @@ function closemenu(){
     menu.style.right = '-200px';
 }
 
-let slides = document.querySelectorAll(".bg-carousel .slide");
+let slides = document.querySelectorAll(".slide");
 let index = 0;
 
-function changeSlide() {
-    slides[index].classList.remove("active");
-    index = (index + 1) % slides.length;
-    slides[index].classList.add("active");
-}
-
-setInterval(changeSlide, 4000); // 4 sec per slide
